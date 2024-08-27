00566a0494a31177c12d58873afc51683ae84a8f
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2024-08-27 10:43:22 -0300

    realocado zoom supervisor

--- cadastros/estab.php
+++ cadastros/estab.php
@@ -157,7 +157,7 @@ include_once (__DIR__ . '/../header.php');
     </div>
 
     <!-- MODAIS DE ZOOM -->
-    <?php include ROOT . '/vendas/apoio/zoomsupervisor.php'; ?>
+    <?php include ROOT . '/vendas/zoom/supervisor.php'; ?>
 
     <!-- LOCAL PARA COLOCAR OS JS -->
 
